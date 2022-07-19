34c7678657514dcd42ea384b350dd08f94782496
Rawi Daniel <rawidaniel96@gmail.com>
2022-07-19 10:49:51 -0700

    heap sort algorithm

--- sort.h
+++ sort.h
@@ -31,4 +31,5 @@ void radix_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void bitonic_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
 #endif /* SORTALGORITHM */
