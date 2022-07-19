1b511723a824168b60d49da781d9dec8f0441505
Rawi Daniel <rawidaniel96@gmail.com>
2022-07-19 10:43:32 -0700

    bitonic sort algorithm

--- sort.h
+++ sort.h
@@ -30,4 +30,5 @@ void merge_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void bitonic_sort(int *array, size_t size);
 #endif /* SORTALGORITHM */
